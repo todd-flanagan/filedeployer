86d9a138b66bc377d33e91fffc772c6aec491f8b
Todd Flanagan <tflanagan@gmail.com>
2018-06-14 16:43:57 -0400

    S3 file download

--- src/main/java/filedeployer/FileDeployerController.java
+++ src/main/java/filedeployer/FileDeployerController.java
@@ -3,14 +3,19 @@ package filedeployer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import filedeployer.services.S3Services;
+import filedeployer.services.S3ServicesImpl;
 
 @RestController
 public class FileDeployerController {
 
-
+  @Autowired
+  S3Services s3Services;
 
     @RequestMapping("/deploy")
-    public String deployFiles(@RequestParam(value="name", defaultValue="World") String name) {
-        return "foo";
+    public void deployFiles(@RequestParam(value="name", defaultValue="World") String name) {
+         s3Services.downloadFile("foo.mltbx");
+        return;
     }
 }
---
+++ src/main/java/filedeployer/services/S3Services.java
@@ -0,0 +1,5 @@
+package filedeployer.services;
+
+public interface S3Services {
+	public void downloadFile(String keyName);
+}
